de96401f9c9e299aca1d2230a17fb8ebf4a6fe94
MJBackes <mjohnbackes@gmail.com>
2020-01-31 09:03:27 -0600

    Added a LoggerManager class implementing the ILoggerManager interface to log messages to logfiles.

--- BookSiteAPI/Startup.cs
+++ BookSiteAPI/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using BookSiteAPI.Extensions;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
 
 namespace BookSiteAPI
 {
@@ -18,6 +20,7 @@ namespace BookSiteAPI
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             Configuration = configuration;
         }
 
@@ -28,6 +31,7 @@ namespace BookSiteAPI
         {
             services.ConfigureCors();
             services.ConfigureIISIntegration();
+            services.ConfigureLoggerService();
 
             services.AddControllers();
         }
